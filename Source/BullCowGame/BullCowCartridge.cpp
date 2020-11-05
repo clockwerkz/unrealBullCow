549102df26e4c792b84469400c2c2729eef5052d
Carlos <clockwerkz@gmail.com>
2020-11-04 18:15:23 -0800

    adding in some print strings for the terminal

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,12 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to Cows!"));
+    PrintLine(TEXT("Please press <Enter> to continue:"));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
+    FString HiddenWord = TEXT("dialogue");
 }
