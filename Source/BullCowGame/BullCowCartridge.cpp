8d3191111c642d15fb6bf1731604641878f28c53
Carlos <clockwerkz@gmail.com>
2020-11-04 19:55:30 -0800

    basic game loop

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,15 +1,26 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
+FString HiddenWord;
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to Cows!"));
     PrintLine(TEXT("Please press <Enter> to continue:"));
+    HiddenWord = TEXT("dialogue");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("dialogue");
+    PrintLine("Hidden word is: " + HiddenWord);
+    if (Input != "") {
+        if (Input == HiddenWord)
+        {
+            PrintLine("You guessed correct!");
+            HiddenWord = "";
+        } else {
+            PrintLine("Sorry! Wrong guess!");
+        }
+    }
 }
